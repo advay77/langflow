308e3f50f94693b633869945ebfa5aa61343940e
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2024-11-02 19:02:26 -0300

    Update parameterId to "tool_mode" in nodeToolbarComponent

--- src/frontend/src/pages/FlowPage/components/nodeToolbarComponent/index.tsx
+++ src/frontend/src/pages/FlowPage/components/nodeToolbarComponent/index.tsx
@@ -335,7 +335,7 @@ export default function NodeToolbarComponent({
   const postToolModeValue = usePostTemplateValue({
     node: data.node!,
     nodeId: data.id,
-    parameterId: name,
+    parameterId: "tool_mode",
   });
 
   return (
